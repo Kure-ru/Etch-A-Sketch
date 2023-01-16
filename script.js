af48f185aa6f9474ad8cc5ee85de69d750848fce
Kure-ru <clairedochez@gmail.com>
2023-01-16 17:31:10 +0900

    changed UI. added hovering function changing the color to pink. added a button to change size of the board from 1 to 100. TO DO : add a rainbow effect and lower opacity to 10%

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
-function createDivs () {
-    let num = 16,
-    div,
+const defaultSize = 16;
+
+function createSquares (size) {
     board = document.getElementById('container')
     fragment = document.createDocumentFragment();
+
 //rows
-   for (let i = 0; i < num; i++){
+   for (let i = 0; i < size; i++){
 //columns
-    for (let j = 0; j < num; j++) {
-        div = document.createElement('div');
-        div.className = 'square';
+    for (let j = 0; j < size; j++) {
+        const square = document.createElement('div');
+        square.className = 'square';
     
-        fragment.appendChild(div);
+        fragment.appendChild(square);
+        square.addEventListener('mouseover', changeColor)
     }
  }
  board.appendChild(fragment);
+ 
+board.style.display = "grid";
+board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
 };
 
-createDivs();
-
 //hovering effect changing the color
+function changeColor(e) {
+  e.target.style.backgroundColor = "pink";
+}
 
-const pixels = document.querySelectorAll(".square")
+//button for changing size
+let btn = document.getElementById("btn");
+btn.addEventListener('click', event => {
+ let num = prompt("choisissez un nombre entre 1 et 100");
+ 
+ if (num <= 100 && num > 0) {
+  container.innerHTML = "";
+  createSquares(num);
+} else {
+  alert("erreur: entrez un nombre compris entre 1 et 100")
+}
+});
 
-// function changeColor() {
-//    pixels.style.backgroundColor = "blue";
-// }
-
-function changeColor() {
-  pixels.forEach(pixel => {
-    pixel.style.backgroundColor = "green";
-  })
- }
-
-pixels.forEach(pixel => pixel.addEventListener('mouseover', changeColor));
+window.onload = () => {
+  createSquares(defaultSize)}
